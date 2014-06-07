8d3efdff8e98d9ba1fb9f0bb826c41b8caf99831
Andrew Darqui <andrew.darqui@gmail.com>
2014-06-06 23:30:46 -0400

    redis

---
+++ src/System/DevUtils/Redis/Include.hs
@@ -0,0 +1,146 @@
+module System.DevUtils.Redis.Include (
+) where
+
+import System.DevUtils.Sys.Multiplex
+import System.DevUtils.Sys.Memory
+
+data Status = OK deriving (Show)
+data RoleType = MASTER | SLAVE deriving (Show)
+
+data Info = Info {
+ _server :: Server,
+ _client :: Client,
+ _memory :: Memory,
+ _persistence :: Persistence,
+ _role :: Role,
+ _cpu :: CPU,
+ _keyspace :: Keyspace,
+ _cluster :: Cluster
+} deriving (Show)
+
+data Server = Server {
+ -- Server
+ _redisVersion :: String,
+ _redisGitSha1 :: String,
+ _redisGitDirty :: Integer,
+ _os :: String,
+ _archBits :: Integer,
+ _multiplexingApi :: Multiplexer,
+ _gccVersion :: String,
+ _processId :: Integer,
+ _runId :: String,
+ _tcpPort :: Integer,
+ _uptimeInSeconds :: Integer,
+ _uptimeInDays :: Integer,
+ _hz :: Integer
+} deriving (Show)
+
+data Client = Client {
+ _connectedClients :: Integer,
+ _clientLongestOutputList :: Integer,
+ _clientBBiggestInputBuf :: Integer,
+ _blockedClients :: Integer
+} deriving (Show)
+
+data Memory = Memory {
+ _usedMemory :: Memory,
+ _usedMemoryHuman :: Memory,
+ _usedMemoryRss :: Memory,
+ _usedMemoryPeak :: Memory,
+ _usedMemoryPeakHuman :: Memory,
+ _memFragmentationRatio :: Float,
+ _memAllocator :: String
+} deriving (Show)
+
+data Persistence = Persistent {
+ _rdbChangesSinceLastSave :: Integer,
+ _rdbBgSaveInProgress :: Bool,
+ _rdbLastSaveTime :: String, -- fix
+ _rdbLastBgSaveStatus :: Status,
+ _rdbCurrentBgSaveTimeSec :: Maybe Integer,
+ _aofEnabled :: Bool,
+ _aofRewriteInProgress :: Bool,
+ _aofLastRewriteTimeSec :: Maybe Integer,
+ _aofCurrentRewriteTimeSec :: Maybe Integer,
+ _aofLastBgrewriteStatus :: Status,
+ _aof :: Maybe AOF
+} deriving (Show)
+
+{- Maybe aof -}
+{-
+ - aof_current_size: AOF current file size
+ - aof_base_size: AOF file size on latest startup or rewrite
+ - aof_pending_rewrite: Flag indicating an AOF rewrite operation will be scheduled once the on-going RDB save is complete.
+ - aof_buffer_length: Size of the AOF buffer
+ - aof_rewrite_buffer_length: Size of the AOF rewrite buffer
+ - aof_pending_bio_fsync: Number of fsync pending jobs in background I/O queue
+ - aof_delayed_fsync: Delayed fsync counter
+ - -}
+data AOF = AOF {
+ _aofCurrentSize :: Integer,
+ _aofBaseSize :: Integer,
+ _aofPendingRewrite :: Bool,
+ _aofBufferLength :: Integer,
+ _aofRewriteBufferLength :: Integer,
+ _aofPendingBioFsync :: Integer,
+ _aofDelayedFsync :: Integer
+} deriving (Show)
+
+data Stats = Stats {
+ _totalConnectionsReceived :: Integer,
+ _totalCommandsProcessed :: Integer,
+ _instantaneousOpsPerSec :: Integer,
+ _rejectedConnections :: Integer,
+ _expiredKeys :: Integer,
+ _evictedKeys :: Integer,
+ _keyspaceHits :: Integer,
+ _keyspaceMisses :: Integer,
+ _pubsubChannels :: Integer,
+ _pubsubPatterns :: Integer,
+ _latestForkUsec :: Integer
+} deriving (Show)
+
+data Role = Role {
+ _role' :: RoleType,
+ _connectedSlaves :: Integer
+} deriving (Show)
+
+{-
+ - If the instance is a slave, these additional fields are provided:
+ - master_host: Host or IP address of the master
+ - master_port: Master listening TCP port
+ - master_link_status: Status of the link (up/down)
+ - master_last_io_seconds_ago: Number of seconds since the last interaction with master
+ - master_sync_in_progress: Indicate the master is SYNCing to the slave
+ - If a SYNC operation is on-going, these additional fields are provided:
+ - master_sync_left_bytes: Number of bytes left before SYNCing is complete
+ - master_sync_last_io_seconds_ago: Number of seconds since last transfer I/O during a SYNC operation
+ - If the link between master and slave is down, an additional field is provided:
+ - master_link_down_since_seconds: Number of seconds since the link is down
+ - The following field is always provided:
+ - connected_slaves: Number of connected slaves
+ - For each slave, the following line is added:
+ - slaveXXX: id, ip address, port, state
+ -}
+
+data CPU = CPU {
+ _usedCpuSys :: Float,
+ _usedCpuUser :: Float,
+ _usedCpuSysChildren :: Float,
+ _usedCpuUserChildren :: Float
+} deriving (Show)
+
+data Keyspace = Keyspace {
+ _databases :: [String]
+} deriving (Show)
+
+data Cluster = Cluster {
+ _clusterEnabled :: Bool
+} deriving (Show)
+
+data CommandStats = CommandStats {
+ _type :: String,
+ _numCalls :: Integer,
+ _totalCpu :: Float,
+ _avgCpu :: Float
+} deriving (Show)
