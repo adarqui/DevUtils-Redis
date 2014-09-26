c92538b1ece969e171d04b3becc77c3ea3867918
Andrew Darqui <andrew.darqui@gmail.com>
2014-09-26 14:33:36 -0400

    urlToConnectInfo ... some todos in there

---
+++ src/System/DevUtils/Redis/Tools.hs
@@ -0,0 +1,41 @@
+module System.DevUtils.Redis.Tools (
+ urlToConnInfo
+) where
+
+import System.DevUtils.Base.Url.Redis
+ (Redis(..))
+
+import System.DevUtils.Base.Url.Session
+ (Session(..))
+
+import System.DevUtils.Base.Url.Auth
+ (Auth(..))
+
+import System.DevUtils.Base.Url.Connection
+ (Connection(..), ConnectionType(..))
+
+import Database.Redis
+ (ConnectInfo(..), PortID(..))
+
+import Data.Maybe
+ (isNothing, fromJust, maybe)
+
+import qualified Data.ByteString.Char8 as C
+
+urlToConnInfo :: Redis -> ConnectInfo
+urlToConnInfo url = ConnInfo {
+  connectHost = tcp,
+  connectPort = PortNumber 6379,
+-- FIX
+--  connectPort = port,
+  connectAuth = auth
+ }
+ where
+  db  = _db
+  port = _port $ _con $ _ses url
+  auth = case (_auth $ _ses url) of
+   (Just v) ->  Just $ C.pack $ _user v
+   _ -> Nothing
+  (tcp,unix) = let (t,d) = (_type $ _con $ _ses url, _dest $ _con $ _ses url) in case t of
+   UNIX -> ("", d)
+   _ -> (d, "")
