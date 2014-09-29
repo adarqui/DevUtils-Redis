64f566aee3779e00694ecb13c449d0741daadaaa
Andrew Darqui <andrew.darqui@gmail.com>
2014-09-28 22:17:32 -0400

    tojson

--- src/System/DevUtils/Redis/Helpers/CommandStats/JSON.hs
+++ src/System/DevUtils/Redis/Helpers/CommandStats/JSON.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE OverloadedStrings, RecordWildCards #-}
 module System.DevUtils.Redis.Helpers.CommandStats.JSON (
  CommandStat(..),
  CommandStats(..),
@@ -21,12 +21,14 @@ import Data.Aeson
 instance FromJSON CommandStat
 instance ToJSON CommandStat
 
-{-
-instance FromJSON CommandStats
-instance ToJSON CommandStats
--}
-
 unMarshall'Value :: [CommandStat] -> Value
 unMarshall'Value cs = object $ map (\v -> (T.pack (_type v), cs'to'Value v)) cs
 
-cs'to'Value cs = Bool True
+cs'to'Value CommandStat{..} =
+ object
+  [
+   ("_type", toJSON _type),
+   ("_numCalls", toJSON _numCalls),
+   ("_totalCpu", toJSON _totalCpu),
+   ("_avgCpu", toJSON _avgCpu)
+  ]
