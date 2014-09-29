e96cc86ecdbf4f4347aa270e144a3ef5079816d3
Andrew Darqui <andrew.darqui@gmail.com>
2014-09-28 21:57:48 -0400

    need a value type for sexier json

--- src/System/DevUtils/Redis/Helpers/CommandStats/JSON.hs
+++ src/System/DevUtils/Redis/Helpers/CommandStats/JSON.hs
@@ -1,15 +1,30 @@
+{-# LANGUAGE OverloadedStrings #-}
 module System.DevUtils.Redis.Helpers.CommandStats.JSON (
  CommandStat(..),
- CommandStats(..)
+ CommandStats(..),
+ unMarshall'Value
 ) where
 
 import System.DevUtils.Redis.Helpers.CommandStats.Include (CommandStat(..), CommandStats(..))
 
+import System.DevUtils.Redis.Helpers.CommandStats.Include (CommandStat(..))
+import System.DevUtils.Base.Data.Map (kvListToMap)
+import System.DevUtils.Parser.Lines.ByteString (runLines, defaultLines)
+
+import Data.Maybe (Maybe(..), catMaybes)
+
 import qualified Data.ByteString as B
-import Data.Aeson (FromJSON, ToJSON, decode, encode)
+import qualified Data.Text as T
+
+import Data.Aeson
 
 instance FromJSON CommandStat
 instance ToJSON CommandStat
 
 instance FromJSON CommandStats
 instance ToJSON CommandStats
+
+unMarshall'Value :: [CommandStat] -> Value
+unMarshall'Value cs = object $ map (\v -> (T.pack (_type v), cs'to'Value v)) cs
+
+cs'to'Value cs = Bool True
